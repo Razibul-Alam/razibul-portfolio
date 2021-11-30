9e46cc245f4d0bcbfa1f1e5bb3178920aefb2335
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-11-30 09:20:24 +0600

    projects done

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -5,18 +5,13 @@ const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand href="#home">Md Razibul</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+    <Nav className="ms-auto">
+      <Nav.Link href="#features">Home</Nav.Link>
+      <Nav.Link href="#pricing">Contact</Nav.Link>
+      <Nav.Link href="#pricing">Projects</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
--- src/Pages/Projects/SingleProject.js
+++ src/Pages/Projects/SingleProject.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import {Button,Card,Col} from 'react-bootstrap'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const SingleProject = ({project}) => {
-    const{title,img,desc,id}=project;
+    const{title,img,live,id}=project;
     return (
         <Col>
         <Card>
           <Card.Img variant="top" src={img} />
           <Card.Body>
             <Card.Title>{title}</Card.Title>
-            <Card.Text>
-             {desc}
-            </Card.Text>
             <div className="d-flex justify-content-between align-items-center">
-            <Link to={`/booking/${id}`}><Button>Book Ticket</Button></Link>
+            <a style={{ color: 'black' }} href={live} target="_blank">Live-Site</a>
+            <Link to={`/booking/${id}`}>Source</Link>
+            <Link to={`/booking/${id}`}>Details</Link>
             </div>
           </Card.Body>
           
